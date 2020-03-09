7411c244f8c51e915a8d34da25f4ba7ef4f9d799
Baledent <jeremy.baledent@laplateforme.io>
2020-03-09 15:28:31 +0100

    job04

---
+++ jour01/job04/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<head>
+
+</head>
+<title>job04</title>
+  <script type="text/javascript" src="script.js"></script>
+<body>
+  <a href='javascript:bisextile(2020)'>Annee</a>
+</body>
+  
